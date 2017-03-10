db72c2766535ffc42eab32c09b4c41a828e234df
dvtam <dvtam95@gmail.com>
2017-03-10 09:06:14 +0700

    tam

--- BabiCare_Server/src/Main/BabiCare_Server.java
+++ BabiCare_Server/src/Main/BabiCare_Server.java
@@ -5,7 +5,11 @@
  */
 package Main;
 
+import Data.Config;
 import Notification.FCMNotification;
+import java.net.ServerSocket;
+import Network.Connect;
+import java.io.IOException;
 
 /**
  *
@@ -16,9 +20,17 @@ private static FCMNotification mNotification;
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        mNotification=new FCMNotification();
-        
+    public static void main(String[] args) throws IOException {
+        //mNotification=new FCMNotification();
+         System.out.println("Server is running . . . \nPORT: "+Config.PORT);
+       ServerSocket listener = new ServerSocket(Config.PORT);
+        try {
+            while (true) {
+                new Connect.Handler(listener.accept()).start();
+            }
+        } finally {
+            listener.close();
+        }
     }
     
 }
